ce5923bdb743fa72c5a2204cb2189b7b012a473e
batprinter <shaharbenharoe@gmail.com>
2024-02-18 19:05:11 +0200

    commit

--- 5.py
+++ 5.py
@@ -2,7 +2,12 @@ my_name = "shahar"
 prefix ="Hello "
 print(list(range(2, 8)))
 print(prefix + my_name)
+
 print(prefix + my_name)
 print(prefix + my_name)
 print(prefix + my_name)
-print(prefix + my_name)
+ccdd
+
+#mocedo
+
+
